6074a61efe5dec7bfeac49514f87b67d2fdc98b4
Imanol <imano.arias@gmail.com>
2025-01-22 22:45:09 -0300

    Se agrega experiencia y refactorizo archivos de lenguaje

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -1,53 +1,64 @@
 import { Briefcase } from "@phosphor-icons/react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+
+function WorkExperienceItem({ title, duration, company, technologies, description }) {
+   return (
+      <section className="mb-8">
+         <div className="flex justify-between items-center">
+            <h1 className="text-lg font-semibold">&bull; {title}</h1>
+            <p className="text-sm">{duration}</p>
+         </div>
+         <p>{company}</p>
+
+         <ul className="flex gap-2 flex-wrap">
+            {technologies.map((tech, index) => (
+               <li key={index} className="bg-gray-100 dark:bg-gray-900 rounded p-1">{tech}</li>
+            ))}
+         </ul>
+
+         <p className="mt-2 text-gray-700 dark:text-gray-400">{description}</p>
+      </section>
+   );
+}
 
 function WorkExperience() {
-      const { t } = useTranslation();
+   const { t } = useTranslation();
 
-      return (
-            <section className="mb-12 animate-fade-left animate-duration-500 animate-delay-1000 animate-ease-linear">
-                  <h1 className="text-2xl font-bold mb-2 flex items-center gap-2 "><Briefcase /> {t('work_experience')}</h1>
+   const experiences = [
+      {
+         title: t("experience_1"),
+         duration: t("duration_exp_1"),
+         company: t("company_1"),
+         technologies: ["ReactJS", "TailwindCSS", "Git"],
+         description: t("description_exp_1"),
+      },
+      {
+         title: t("experience_2"),
+         duration: t("duration_exp_2"),
+         company: t("company_2"),
+         technologies: ["ReactJS", "TailwindCSS", "Git", "PostgreSQL", "NodeJS", "Express"],
+         description: t("description_exp_2"),
+      },
+      {
+         title: t("experience_3"),
+         duration: t("duration_exp_3"),
+         company: t("company_3"),
+         technologies: ["NodeJS", "Hono", "Cron", "NodeMailer", "Git", "SOAP", "Docker"],
+         description: t("description_exp_3")
+      }
+   ];
 
-                  <section className="mb-8">
-                        <div className="flex justify-between items-center">
-                              <h1 className="text-lg font-semibold">&bull; {t('frontend_dev')}</h1>
-                              <p className="text-sm">{t('duration_frontend')}</p>
-                        </div>
-                        <p>{t('company_frontend')}</p>
+   return (
+      <section className="mb-12 animate-fade-left animate-duration-500 animate-delay-1000 animate-ease-linear">
+         <h1 className="text-2xl font-bold mb-2 flex items-center gap-2">
+            <Briefcase /> {t("work_experience")}
+         </h1>
 
-                        <ul className="flex gap-2 flex-wrap">
-                              <li className="bg-gray-100 dark:bg-gray-900 rounded p-1">ReactJS</li>
-                              <li className="bg-gray-100 dark:bg-gray-900 rounded p-1">TailwindCSS</li>
-                              <li className="bg-gray-100 dark:bg-gray-900 rounded p-1">Git</li>
-                        </ul>
-
-                        <p className="mt-2 text-gray-700 dark:text-gray-400">
-                              {t('description_frontend')}
-                        </p>
-                  </section>
-
-                  <section>
-                        <div className="flex justify-between items-center">
-                              <h1 className="text-lg font-semibold">&bull; {t('fullstack_dev')}</h1>
-                              <p className="text-sm">{t('duration_fullstack')}</p>
-                        </div>
-                        <p>{t('company_fullstack')}</p>
-
-                        <ul className="flex gap-2 flex-wrap">
-                              <li className="bg-gray-100 dark:bg-gray-900 rounded p-1">ReactJS</li>
-                              <li className="bg-gray-100 dark:bg-gray-900 rounded p-1">TailwindCSS</li>
-                              <li className="bg-gray-100 dark:bg-gray-900 rounded p-1">Git</li>
-                              <li className="bg-gray-100 dark:bg-gray-900 rounded p-1">PostgreSQL</li>
-                              <li className="bg-gray-100 dark:bg-gray-900 rounded p-1">NodeJS</li>
-                              <li className="bg-gray-100 dark:bg-gray-900 rounded p-1">Express</li>
-                        </ul>
-
-                        <p className="mt-2 text-gray-700 dark:text-gray-400">
-                              {t('description_fullstack')}
-                        </p>
-                  </section>
-            </section>
-      );
+         {experiences.map((experience, index) => (
+            <WorkExperienceItem key={index} {...experience} />
+         ))}
+      </section>
+   );
 }
 
 export default WorkExperience;
