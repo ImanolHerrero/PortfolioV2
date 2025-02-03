41dfab2b56624119345b437bd2b02d30b4c2b8d0
Imanol <imano.arias@gmail.com>
2025-02-03 13:42:34 -0300

    Se cambia import de imagen

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import WorkExperience from "./components/WorkExperience";
 import Proyects from "./components/Proyects";
 import { useTranslation } from 'react-i18next';
 import { Envelope } from "@phosphor-icons/react";
+import icon from '../public/icon.png'
 
 function App() {
    const { t } = useTranslation();
    return (
       <main className="max-w-screen-md mx-auto min-h-screen p-4 text-slate-900 dark:text-slate-100">
          <nav className="flex justify-between items-center gap-6 py-4 sticky top-0 z-50 bg-[#fcfcfc] dark:bg-zinc-900 border-b">
-            <img src="../public/icon.png" alt="Imanol Herrero Icon" className="h-10 w-10" />
+            <img src={icon} alt="Imanol Herrero Icon" className="h-10 w-10" />
 
             <div className="flex items-center gap-6">
                <a href="mailto:imano.arias@gmail.com">
